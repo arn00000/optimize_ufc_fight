02222342b2287058cae4bb0d41bf491f1c7725f0
Caaron <arn_0000@hotmail.com>
2023-08-06 16:19:17 +0800

    gg

--- app.py
+++ app.py
@@ -10,23 +10,28 @@ def predict_popu(R_KD, B_KD, R_SUB_ATT, B_SUB_ATT, R_CTRL,
        B_CTRL, R_LANDED_HEAD, B_LANDED_HEAD, R_LANDED_GROUND,
        B_LANDED_GROUND,R_LANDED_BODY,B_LANDED_BODY,R_LANDED_LEG,B_LANDED_LEG,
     R_LANDED_CLINCH,B_LANDED_CLINCH,
-       title_bout,B_Stance,R_Stance,B_current_win_streak,R_current_win_streak):
+       title_bout,B_Stance,R_Stance,R_SIG_TD,B_SIG_TD,B_DISTANCE,R_DISTANCE):
     
     prediction = model.predict([[R_KD, B_KD, R_SUB_ATT, B_SUB_ATT, R_CTRL,
        B_CTRL, R_LANDED_HEAD, B_LANDED_HEAD, R_LANDED_GROUND,
        B_LANDED_GROUND,R_LANDED_BODY,B_LANDED_BODY,R_LANDED_LEG,B_LANDED_LEG,
     R_LANDED_CLINCH,B_LANDED_CLINCH,
-       title_bout,B_Stance,R_Stance,B_current_win_streak,R_current_win_streak]])
+       title_bout,B_Stance,R_Stance,R_SIG_TD,B_SIG_TD,B_DISTANCE,R_DISTANCE]])
     return prediction[0]
 
 def main():
     st.image('https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/UFC_Logo.svg/330px-UFC_Logo.svg.png',width=300)
     st.title("Optimize UFC fights for Betting")
+    st.text('Enter both figther previous fight stats to predict their upcoming fight winner.')
     title_bout = st.selectbox(
-            'Is this title bout',
+            'Is this Title Bout',
             (False, True))
 
-    col1, col2, = st.columns(2)
+    col1, col2 = st.columns(2)
+    with col1:
+        Red = st.text_input('Red corner figther name', 'Red Corner')
+    with col2:
+        Blue = st.text_input('Blue corner figther name', 'Blue Corner')
     with col1:
         R_Stance = st.selectbox(
             'Select Red fight stance',
@@ -35,42 +40,46 @@ def main():
         B_Stance = st.selectbox(
             'Select Blue fight stance',
             ('Orthodox', 'Southpaw', 'Switch'))
-    with col1:      
-        R_current_win_streak = st.number_input("R_current_win_streak",min_value=0, step=1)
-    with col2:
-        B_current_win_streak = st.number_input("B_current_win_streak",min_value=0, step=1)
     with col1:
-        R_KD = st.number_input("R_How many KO in this fight?",min_value=0, step=1)
+        R_KD = st.number_input("R_How many KO in last fight?",min_value=0, step=1)
     with col2:
-        B_KD = st.number_input("B_How many KO in this fight?",min_value=0, step=1)
-    with col1:
-        R_SUB_ATT = st.slider('R_Submission attemp', 0, 200)
-    with col2:
-        B_SUB_ATT = st.slider('B_Submission attemp', 0, 200)
+        B_KD = st.number_input("B_How many KO in last fight?",min_value=0, step=1)
     with col1: 
-        R_CTRL = st.slider('R_Control time by second', 0, 200)
+        R_CTRL = st.number_input('R_Control time by second',min_value=0, step=1)
     with col2:
-        B_CTRL = st.slider('B_Control time by second', 0, 200)
+        B_CTRL = st.number_input('B_Control time by second',min_value=0, step=1)
     with col1:
-        R_LANDED_HEAD = st.slider('R_Significant strike on head', 0, 200)
+        R_SUB_ATT = st.slider('R_Submission attemp', 0, 10)
     with col2:
-        B_LANDED_HEAD = st.slider('B_Significant strike on head', 0, 200)
+        B_SUB_ATT = st.slider('B_Submission attemp', 0, 10)
     with col1:
-        R_LANDED_GROUND = st.slider('R_Ground and Pound', 0, 200)
+        R_SIG_TD = st.slider('R_Take down attemp', 0, 20)
     with col2:
-        B_LANDED_GROUND = st.slider('B_Ground and Pound', 0, 200)
+        B_SIG_TD = st.slider('B_Take down attemp', 0, 20)
     with col1:
-        R_LANDED_BODY = st.slider('R_Significant strike on body', 0, 200)
+        R_LANDED_GROUND = st.slider('R_Ground and Pound', 0, 100)
     with col2:
-        B_LANDED_BODY = st.slider('B_Significant strike on body', 0, 200)
+        B_LANDED_GROUND = st.slider('B_Ground and Pound', 0, 100)
     with col1:
-        R_LANDED_LEG = st.slider('R_Significant strike on leg', 0, 200)
+        R_LANDED_HEAD = st.slider('R_Significant strike on head', 0, 100)
     with col2:
-        B_LANDED_LEG = st.slider('B_Significant strike on leg', 0, 200)
+        B_LANDED_HEAD = st.slider('B_Significant strike on head', 0, 100)
     with col1:
-        R_LANDED_CLINCH = st.slider('R_Clinch attemp', 0, 200)
+        R_LANDED_BODY = st.slider('R_Significant strike on body', 0, 100)
     with col2:
-        B_LANDED_CLINCH = st.slider('B_Clinch attemp', 0, 200)
+        B_LANDED_BODY = st.slider('B_Significant strike on body', 0, 100)
+    with col1:
+        R_LANDED_LEG = st.slider('R_Significant strike on leg', 0, 100)
+    with col2:
+        B_LANDED_LEG = st.slider('B_Significant strike on leg', 0, 100)
+    with col1:
+        R_LANDED_CLINCH = st.slider('R_Clinch attemp', 0, 50)
+    with col2:
+        B_LANDED_CLINCH = st.slider('B_Clinch attemp', 0, 50)
+    with col1:
+        R_DISTANCE = st.slider('R_DISTANCE', 0, 150)
+    with col2:
+        B_DISTANCE = st.slider('B_DISTANCE', 0, 150)
         
     result=''
     st.text("Prediction accuracy up to 87%")
@@ -96,11 +105,11 @@ def main():
            B_CTRL, R_LANDED_HEAD, B_LANDED_HEAD, R_LANDED_GROUND,
            B_LANDED_GROUND,R_LANDED_BODY,B_LANDED_BODY,R_LANDED_LEG,B_LANDED_LEG,
             R_LANDED_CLINCH,B_LANDED_CLINCH,
-           title_bout,B_Stance,R_Stance,B_current_win_streak,R_current_win_streak)
+           title_bout,B_Stance,R_Stance,R_SIG_TD,B_SIG_TD,B_DISTANCE,R_DISTANCE)
         if result == 2:
-            result = 'The winner from Red corner'
+            result = 'The winner is ' + Red
         elif result == 0:
-            result = 'The winner from Blue corner'
+            result = 'The winner is ' + Blue
         elif result == 1:
             result = 'Is a Draw'
         st.success(format(result))
